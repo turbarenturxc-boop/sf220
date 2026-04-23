19a08099bc0f5c4e70eea6d4774a350b59488466
Sonekraros <turbarenturxc@gmail.com>
2026-04-23 09:07:04 +0700

    update code 1

--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+import { Link } from 'react-router-dom'
 
 function Hero() {
     return (
         <div className='flex flex-col items-center justify-center mt-20 gap-9 px-4 max-w-5xl mx-auto'>
             <h1 className='font-extrabold text-[40px] md:text-[60px] text-center leading-tight'>
-                <span>Discover Your Next Adventure with Ai:</span><br />Personalized Travel Recommendations</h1>
+                <span className='text-[#f56551]'>Discover Your Next Adventure with Ai</span><br />Personalized Travel Recommendations</h1>
+
+            <Link to={'/create-trip'}>
+                <Button>Get Started, For Free</Button>
+            </Link>
         </div>
     )
 }
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function CreateTrip() {
     return (
-        <h2>Hello this is second page</h2>
+        <div className="flex flex-col items-center justify-center min-h-[80vh] text-center px-4">
+            <h2 className="text-4xl md:text-6xl font-bold">Tell us your travel preferences</h2>
+            <p className='m-3 text-gray-500'>Write more here!</p>
+
+            <div className='mt-20'>
+                <div>
+                    <h2 className='text-xl my-3 font-medium'>What is your place to go?</h2>
+                </div>
+            </div>
+        </div>
     )
 }
 
